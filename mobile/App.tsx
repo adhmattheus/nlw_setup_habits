5fbf5e70458654422d40c8bdbe088b1821617124
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-01-30 12:20:19 -0300

    fianlizando layout da home - mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,47 +1,22 @@
-
-import { ImageBackground, StatusBar, StyleSheet, Text, View } from 'react-native';
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold
-} from '@expo-google-fonts/inter';
-
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_800ExtraBold
-  });
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold });
 
   if (!fontsLoaded) {
     return (
       <Loading />
     );
-  }
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Iniciando mobile!</Text>
+    <>
+      <Home />
       < StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
+};
