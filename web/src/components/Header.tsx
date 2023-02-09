ab6cb3c8511347eb6626c6fcc0f68efd8817aafc
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-02-08 22:32:22 -0300

    botao de modal

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import logoImage from '../assets/logo.svg';
-import { Plus } from 'phosphor-react';
+import { Plus, X } from 'phosphor-react';
 import * as Dialog from '@radix-ui/react-dialog'
 
 export function Header() {
@@ -18,11 +18,14 @@ export function Header() {
         <Dialog.Portal>
           <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0' />
           <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+            <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover::text-zinc-200'>
+              <X size={24} aria-label="Fechar" />
+            </Dialog.Close>
             Conteúdo do modal
           </Dialog.Content >
         </Dialog.Portal>
       </Dialog.Root >
+      
     </div>
-
   );
 };
