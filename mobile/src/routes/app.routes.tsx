b77d96551dba72407de69169c4a124d3f1999c3d
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-02-15 16:49:05 -0300

    finalizando mobile e web do app

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,29 +1,28 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Habit } from '../screens/Habit';
-import { Home } from '../screens/Home';
-import { New } from '../screens/New';
 
 const { Navigator, Screen } = createNativeStackNavigator();
+
+import { Home } from '../screens/Home';
+import { New } from '../screens/New';
+import { Habit } from '../screens/Habit';
 
 export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-
       <Screen
-        name='home'
+        name="home"
         component={Home}
       />
 
       <Screen
-        name='habit'
-        component={Habit}
-      />
-
-      <Screen
-        name='new'
+        name="new"
         component={New}
       />
 
+      <Screen
+        name="habit"
+        component={Habit}
+      />
     </Navigator>
-  );
+  )
 }
